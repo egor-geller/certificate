b6ed0f354dc91bd1bdea44b1b1784e20d8a445d6
Egor Geller <Egkingers11@gmail.com>
2021-10-09 15:16:01 +0300

    Update: java doc

--- web/src/main/java/com/epam/esm/controller/TagController.java
+++ web/src/main/java/com/epam/esm/controller/TagController.java
@@ -1,6 +1,10 @@
 package com.epam.esm.controller;
 
 import com.epam.esm.dto.TagDto;
+import com.epam.esm.entity.Tag;
+import com.epam.esm.exception.EntityAlreadyExistsException;
+import com.epam.esm.exception.EntityNotFoundException;
+import com.epam.esm.exception.InvalidEntityException;
 import com.epam.esm.service.TagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,34 +13,73 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+/**
+ * Class containing public REST API endpoints related to {@link Tag} entity.
+ *
+ * @author Egor Geller
+ */
 @RestController
 @RequestMapping("/api/tags")
 public class TagController {
     private final TagService tagService;
 
+    /**
+     * Instantiates a new Tag controller.
+     *
+     * @param tagService the tag service
+     */
     @Autowired
     public TagController(TagService tagService) {
         this.tagService = tagService;
     }
 
+    /**
+     * Gets all tags.
+     *
+     * @return JSON {@link ResponseEntity} object that contains list of {@link TagDto}
+     */
     @GetMapping
     public ResponseEntity<List<TagDto>> getAllTags() {
         List<TagDto> tagDtoList = tagService.findAllTagsService();
         return new ResponseEntity<>(tagDtoList, HttpStatus.OK);
     }
 
+    /**
+     * Gets tag by its unique id
+     *
+     * @param id tag id
+     * @return JSON {@link ResponseEntity} object that contains {@link TagDto} object
+     * @throws EntityNotFoundException in case certificate with this id doesn't exist
+     */
     @GetMapping("/{id}")
     public ResponseEntity<TagDto> getTag(@PathVariable("id") Long id) {
         TagDto tagByIdService = tagService.findTagByIdService(id);
         return new ResponseEntity<>(tagByIdService, HttpStatus.OK);
     }
 
+    /**
+     * Create new tag entity.
+     *
+     * @param tagDto {@link TagDto} instance
+     * @return JSON {@link ResponseEntity} object that contains status {@code HttpStatus.CREATED}
+     * @throws InvalidEntityException       when the content of {@link TagDto} instance is not correctly written
+     * @throws EntityAlreadyExistsException when {@link Tag} entity is already exists
+     */
     @PostMapping
     public ResponseEntity<Void> createTag(@RequestBody TagDto tagDto) {
         tagService.createTagService(tagDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    /**
+     * Delete existing tag.
+     *
+     * @param id tag id
+     * @return {@code HttpStatus.OK} and {@code True} when entity has been deleted, otherwise,
+     * {@code HttpStatus.NOT_MODIFIED} and {@code False}
+     * @throws InvalidEntityException when id is not written correctly
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Boolean> deleteTag(@PathVariable("id") Long id) {
         boolean hasBeenDeleted = tagService.deleteTagService(id);
