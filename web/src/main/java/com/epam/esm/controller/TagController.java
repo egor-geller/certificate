4fcc26188f5d13492db3f24ec5b029c5a44dd7a8
Egor Geller <Egkingers11@gmail.com>
2021-10-09 11:52:35 +0300

    Init

---
+++ web/src/main/java/com/epam/esm/controller/TagController.java
@@ -0,0 +1,15 @@
+package com.epam.esm.controller;
+
+import com.epam.esm.service.TagService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/tags")
+public class TagController {
+    private TagService tagService;
+
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+}
