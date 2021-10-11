dd5c182201a5e9ede69a9af197330800a406704d
Egor Geller <Egkingers11@gmail.com>
2021-10-11 23:57:30 +0300

    Update: made "test" as a constant

--- repository/src/test/java/com/epam/esm/ProfileResolverTest.java
+++ repository/src/test/java/com/epam/esm/ProfileResolverTest.java
@@ -5,9 +5,11 @@ import org.springframework.test.context.ActiveProfilesResolver;
 
 public class ProfileResolverTest implements ActiveProfilesResolver {
 
+    private static final String TEST_PARAMETER = "test";
+
     @Override
     public String[] resolve(Class<?> testClass) {
-        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "test");
-        return new String[] {"test"};
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, TEST_PARAMETER);
+        return new String[] {TEST_PARAMETER};
     }
 }
