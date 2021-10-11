15bc8a7f53666e353520aebdce248cd1f38aa151
Egor Geller <Egkingers11@gmail.com>
2021-10-11 17:18:02 +0300

    Update: fixing customSqlData method

--- repository/src/main/java/com/epam/esm/config/DatabaseConfiguration.java
+++ repository/src/main/java/com/epam/esm/config/DatabaseConfiguration.java
@@ -1,13 +1,13 @@
 package com.epam.esm.config;
 
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -16,25 +16,31 @@ import javax.sql.DataSource;
 @Configuration
 @ComponentScan("com.epam.esm")
 @EnableTransactionManagement
-@PropertySource("classpath:properties/db.properties")
+@PropertySource("classpath:properties/db-prod.properties")
 public class DatabaseConfiguration {
 
-    @Value("database.driver")
+    @Value("${database.driver}")
     private String driverName;
-    @Value("database.user")
+    @Value("${database.user}")
     private String username;
-    @Value("database.password")
+    @Value("${database.password}")
     private String password;
-    @Value("database.url")
+    @Value("${database.url}")
     private String url;
+    @Value("${database.pool.min}")
+    private int minimumPool;
+    @Value("${database.pool.max}")
+    private int maximumPool;
 
     @Bean
     public DataSource customSqlData() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        BasicDataSource dataSource = new BasicDataSource();
         dataSource.setDriverClassName(driverName);
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
+        dataSource.setMinIdle(minimumPool);
+        dataSource.setMaxIdle(maximumPool);
 
         return dataSource;
     }
