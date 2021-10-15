f2929eb8ac6511d8e397750a030e45dd3b32675c
Egor Geller <Egkingers11@gmail.com>
2021-10-15 03:15:17 +0300

    Update: added NamedParameterJdbcTemplate

--- repository/src/main/java/com/epam/esm/config/DatabaseConfiguration.java
+++ repository/src/main/java/com/epam/esm/config/DatabaseConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -48,5 +49,10 @@ public class DatabaseConfiguration {
     @Bean
     public PlatformTransactionManager transactionManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate(DataSource dataSource) {
+        return new NamedParameterJdbcTemplate(dataSource);
     }
 }
