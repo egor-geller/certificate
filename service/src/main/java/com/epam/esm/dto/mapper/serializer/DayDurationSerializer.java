c61697f69b687036dea54915fe2ab7c4c22600a7
Egor Geller <Egkingers11@gmail.com>
2021-11-08 16:09:09 +0300

    Update: moved to com.epam.esm.dto.mapper.serializer

--- service/src/main/java/com/epam/esm/dto/mapper/serializer/DayDurationSerializer.java
+++ service/src/main/java/com/epam/esm/dto/mapper/serializer/DayDurationSerializer.java
@@ -1,4 +1,4 @@
-package com.epam.esm.dto.mapper;
+package com.epam.esm.dto.mapper.serializer;
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
