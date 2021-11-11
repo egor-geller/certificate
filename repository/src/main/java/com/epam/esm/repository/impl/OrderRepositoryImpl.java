773a3e90eaef51054afd3379d6e7df1acce95bca
Egor <egkingers11@gmail.com>
2021-11-11 04:09:52 +0300

    Update: query SELECT_ORDER_BY_USER_ID fix

--- repository/src/main/java/com/epam/esm/repository/impl/OrderRepositoryImpl.java
+++ repository/src/main/java/com/epam/esm/repository/impl/OrderRepositoryImpl.java
@@ -40,10 +40,9 @@ public class OrderRepositoryImpl implements OrderRepository, CreateRepository<Or
         return Optional.ofNullable(entityManager.find(Order.class, id));
     }
 
-    //TODO: how to get o.user.id?
     @Override
     public List<Order> findByUserId(PaginationContext paginationContext, Long id) {
-        return entityManager.createQuery("SELECT o FROM Order o WHERE o.user.id = ?1", Order.class)
+        return entityManager.createQuery(SELECT_ORDER_BY_USER_ID, Order.class)
                 .setParameter(1, id)
                 .setFirstResult(paginationContext.getStartPage())
                 .setMaxResults(paginationContext.getLengthOfContext())
