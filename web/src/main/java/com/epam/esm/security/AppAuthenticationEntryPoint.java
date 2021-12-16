f86dd4de9d338305b38d66ae3ccff54e926e7382
Egor <egkingers11@gmail.com>
2021-12-16 12:36:16 +0300

    Update: refactor commence method

--- web/src/main/java/com/epam/esm/security/AppAuthenticationEntryPoint.java
+++ web/src/main/java/com/epam/esm/security/AppAuthenticationEntryPoint.java
@@ -1,12 +1,8 @@
 package com.epam.esm.security;
 
-import com.epam.esm.controller.ErrorCode;
+import com.epam.esm.controller.CustomExceptionHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -14,48 +10,22 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-
-import static com.epam.esm.controller.ErrorMessages.ERROR_CODE;
-import static com.epam.esm.controller.ErrorMessages.ERROR_MESSAGE;
 
 @Component
 public class AppAuthenticationEntryPoint implements AuthenticationEntryPoint {
-    private static final String UNAUTHORIZED_MESSAGE = "unauthorized";
-    private static final String CONTENT_TYPE_JSON = "application/json";
 
-    private final ResourceBundleMessageSource bundleMessageSource;
+    private final CustomExceptionHandler customExceptionHandler;
 
     @Autowired
-    public AppAuthenticationEntryPoint(ResourceBundleMessageSource bundleMessageSource) {
-        this.bundleMessageSource = bundleMessageSource;
+    public AppAuthenticationEntryPoint(CustomExceptionHandler customExceptionHandler) {
+        this.customExceptionHandler = customExceptionHandler;
     }
 
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
-        String errorMessage = getErrorMessage();
-        ResponseEntity<Object> objectResponseEntity = buildErrorResponseEntity(errorMessage);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        response.setContentType(CONTENT_TYPE_JSON);
-
         ObjectMapper objectMapper = new ObjectMapper();
-        response.getWriter().write(objectMapper.writeValueAsString(objectResponseEntity));
-    }
-
-    private String getErrorMessage() {
-        Locale locale = LocaleContextHolder.getLocale();
-        return bundleMessageSource.getMessage(UNAUTHORIZED_MESSAGE, null, locale);
-    }
-
-    private ResponseEntity<Object> buildErrorResponseEntity(String errorMessage) {
-        Map<String, Object> body = new HashMap<>();
-        body.put(ERROR_MESSAGE, errorMessage);
-        body.put(ERROR_CODE, ErrorCode.CODE_ERROR_401);
-
-        return new ResponseEntity<>(body, HttpStatus.UNAUTHORIZED);
+        response.getWriter().write(objectMapper.writeValueAsString(customExceptionHandler.authenticationExceptionHandle()));
     }
 }
