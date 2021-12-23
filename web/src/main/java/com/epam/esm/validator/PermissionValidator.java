595799134650dc13a734abfcaa2daa2f372daa84
Egor Geller <Egkingers11@gmail.com>
2021-12-24 01:10:17 +0300

    Update: refactor

--- web/src/main/java/com/epam/esm/validator/PermissionValidator.java
+++ web/src/main/java/com/epam/esm/validator/PermissionValidator.java
@@ -1,7 +1,7 @@
 package com.epam.esm.validator;
 
 import com.epam.esm.dto.OrderDto;
-import com.epam.esm.exception.InvalidJsonException;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -12,7 +12,7 @@ public class PermissionValidator {
     public boolean check(Long id, List<OrderDto> someObject) {
         boolean b = someObject.stream().anyMatch(s -> s.getUserId().equals(id));
         if (!b) {
-            throw new InvalidJsonException();
+            throw new AccessDeniedException("Access denied");
         }
         return true;
     }
